6ba7c663b15a060eabbf4ba9f113f70b482c216c
nguyendacthanh <thanhndph23033@fpt.edu.vn>
2025-09-24 17:03:50 +0700

    done

--- Assets/Script/Character/CharacterData.cs
+++ Assets/Script/Character/CharacterData.cs
@@ -1,6 +1,13 @@
 using UnityEngine;
 
-public class CharacterData
+[CreateAssetMenu(menuName = "Characters/CharactersData")]
+public class UnitData : ScriptableObject
 {
+    public string TanCong;
+    public string Mau;
+    public string Speed;
+
+    public int Armor;
+
     
 }
