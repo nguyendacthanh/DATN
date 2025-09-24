cbb8637fed90d1b26ecc07327a627e7f21580dc1
kienvu15 <88vutrunkien@gmail.com>
2025-09-24 17:53:56 +0700

    test deal damage action

--- Assets/Script/Character/CharacterAction.cs
+++ Assets/Script/Character/CharacterAction.cs
@@ -1,6 +1,16 @@
 using UnityEngine;
 
-public class CharacterAction
+public class CharacterAction : MonoBehaviour
 {
-    
+    public bool Attack = false;
+
+    public void Update()
+    {
+        if(Attack == true)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<Character>().Mau -= 10;
+            Attack = false;
+        }
+    }
 }
